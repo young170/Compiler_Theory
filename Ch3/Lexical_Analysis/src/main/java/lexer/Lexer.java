deadfd7c8ac6f4e87ca2eaba25d14fbe6f3a0fcd
young170 <seongbin10209@gmail.com>
2024-03-20 23:49:41 +0900

    update: Extract static utility regex matchers

--- Ch3/Lexical_Analysis/src/main/java/lexer/Lexer.java
+++ Ch3/Lexical_Analysis/src/main/java/lexer/Lexer.java
@@ -18,6 +18,7 @@ public class Lexer {
     }
 
     public Lexer() {
+        // insert constants to symbol table at initialization
         reserve(new Word(Tag.TRUE, "true"));
         reserve(new Word(Tag.FALSE, "false"));
     }
@@ -34,74 +35,102 @@ public class Lexer {
         // load file data into both buffer pairs
         try {
             FileInputStream fileInputStream = new FileInputStream(filePath);
-            lexer.buffers = loadFileIntoBuffers(fileInputStream);
+            lexer.buffers = lexer.loadFileIntoBuffers(fileInputStream);
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + filePath);
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
         }
 
+        // TODO
         // iterate over buffers using lexmeBegin and forward based on FSM
-        iterateBuffer(lexer, lexer.buffers[0]);
-        iterateBuffer(lexer, lexer.buffers[1]);
+        /*
+         * while (forward_ptr has not reached an eof marker) {
+         *      lexer.iterateBuffer(lexer, lexer.buffers);
+         *      *** which would iterate using both buffers as buffer pair
+         * }
+         */
+        lexer.iterateBuffer(lexer, lexer.buffers[0]);
+        lexer.iterateBuffer(lexer, lexer.buffers[1]);
     }
 
-    private static char[][] loadFileIntoBuffers(InputStream inputStream) throws IOException {
+    public char[][] loadFileIntoBuffers(InputStream inputStream) throws IOException {
         char[][] buffers = new char[2][4096];
         InputStreamReader reader = new InputStreamReader(inputStream);
         
         int bytesRead1 = reader.read(buffers[0], 0, 4096);
         int bytesRead2 = reader.read(buffers[1], 0, 4096);
 
+        // TODO
         // if bytesRead<1,2> is less than 4096, then mark eof
+        if (bytesRead1 < buffers[0].length) {
+            buffers[0][bytesRead1] = '\0';
+        } else if (bytesRead2 < buffers[1].length) {
+            buffers[0][bytesRead2] = '\0';
+        }
 
         return buffers;
     }
 
-    private static void iterateBuffer(Lexer lexer, char[] buffer) {
+    private static int matchNumPattern(Lexer lexer, Pattern pattern, char targetChar, char[] buffer, int ptr) {
+        if (pattern.matcher(String.valueOf(targetChar)).matches()) {
+            int v = 0;
+
+            do {
+                v = 10 * v + Character.digit(targetChar, 10);
+                targetChar = buffer[ptr++];
+            } while (Character.isDigit(targetChar));
+
+            Num num = new Num(v);
+            lexer.words.put(Integer.toString(v), num);
+        }
+        
+        return ptr;
+    }
+
+    private static int matchAlphaPattern(Lexer lexer, Pattern pattern, char targetChar, char[] buffer, int ptr) {
+        if (pattern.matcher(String.valueOf(targetChar)).matches()) {
+            StringBuffer b = new StringBuffer();
+
+            do {
+                b.append(targetChar);
+                targetChar = buffer[ptr++];
+            } while (Character.isLetter(targetChar));
+
+            String s = b.toString();
+
+            Word w = (Word) lexer.words.get(s);
+            if (w == null) { // entry already in symbol table
+                w = new Word(Tag.ID, s);
+                lexer.words.put(s, w);
+            }
+        }
+
+        return ptr;
+    }
+
+    public void iterateBuffer(Lexer lexer, char[] buffer) {
         Pattern alphaPattern = Pattern.compile("[a-zA-Z]+");
         Pattern numPattern = Pattern.compile("[0-9]+");
 
-        int lexmeBegin = 0; // Starting pointer to char in buffer
+        int lexmeBegin = 0; // Starting pointer of current token in buffer
+        int forward = 0; // Ending pointer of current token in buffer
         while (lexmeBegin < buffer.length) {
-            char currentChar = buffer[lexmeBegin++];
+            char currentChar = buffer[forward++];
 
-            if (currentChar == ' ' || currentChar == '\n') {
+            if (currentChar == ' ' || currentChar == '\t') {
+                if (lexmeBegin != forward) { // end of token
+                    lexmeBegin = forward;
+                }
+
                 continue;
             } else if (currentChar == '\n') {
                 lexer.lineCount++;
                 continue;
             }
 
-            if (numPattern.matcher(String.valueOf(currentChar)).matches()) {
-                int v = 0;
-
-                do {
-                    v = 10 * v + Character.digit(currentChar, 10);
-                    currentChar = buffer[lexmeBegin++];
-                } while (Character.isDigit(currentChar));
-
-                // create entry of Num in symbol table
-            }
-
-            if (alphaPattern.matcher(String.valueOf(currentChar)).matches()) {
-                StringBuffer b = new StringBuffer();
-
-                do {
-                    b.append(currentChar);
-                    currentChar = buffer[lexmeBegin++];
-                } while (Character.isLetter(currentChar));
-
-                String s = b.toString();
-
-                Word w = (Word) lexer.words.get(s);
-                if (w != null) { // entry already in symbol table
-                    continue;
-                }
-
-                w = new Word(Tag.ID, s);
-                lexer.words.put(s, w);
-            }
+            matchNumPattern(lexer, numPattern, currentChar, buffer, currentChar);
+            matchAlphaPattern(lexer, alphaPattern, currentChar, buffer, currentChar);
         }
     }
 
